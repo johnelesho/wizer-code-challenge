8c5cea8f3e76952b2c9b05331306648f6a5b2649
John Elesho <john@thebulb.africa>
2023-12-28 21:40:05 +0100

    Initial Code Setup
    Project Bootrap

---
+++ src/main/java/tech/elsoft/wizercodechallenge/WizerCodeChallengeApplication.java
@@ -0,0 +1,13 @@
+package tech.elsoft.wizercodechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WizerCodeChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WizerCodeChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/elsoft/wizercodechallenge/WizerCodeChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.elsoft.wizercodechallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WizerCodeChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
